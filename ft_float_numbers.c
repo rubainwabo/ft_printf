709d4650c61a3eaa15862367c130bc867abc6550
Rubain Kamegneson wabo <rkamegne@w-r8-p4.s19.be>
2019-04-30 18:29:49 +0200

    floating

--- ft_float_numbers.c
+++ ft_float_numbers.c
@@ -12,9 +12,38 @@
 
 #include "libftprintf.h"
 
+double  ft_roundf(long double nbr, t_conv *type)
+{
+    double     dec;
+    int         tmp;
+    int         i;
+    int         res;
+
+    i = 0;
+    res = 0;
+    dec = nbr - (long int)nbr;
+    while (i++ < type->precision)
+    {
+        dec -= (int)dec;
+        dec *= 10;
+        tmp = (int)dec;
+        if (i == type->precision)
+        {
+            dec -= (int)dec;
+            dec *= 10;
+            tmp += ((int)dec >= 5) ? 1 : 0;
+        }
+        res *= 10 + tmp;
+    }
+    while (i--)
+        res /= 10;
+    return ((long int)nbr + res);
+}
+
 void    ft_convert_float(long double nbr, t_conv *type)
 {
     (void)nbr;
     if (type->precision == -1)
         type->precision = 6;
+    
 }
